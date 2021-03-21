96c981d4e56e38511e1d8d2f42bd3e1662da522e
Marcin <marcin.konow@lczyk.xyz>
2021-03-21 22:52:12 +0000

    init robustness

--- src/pixel-perfect.cpp
+++ src/pixel-perfect.cpp
@@ -89,6 +89,7 @@ void lcd_init() {
   // // finally, set # lines, font size, etc.
   lcd_command(0x20 | 0x08); // LCD_FUNCTIONSET | LCD_2LINE
   lcd_command(0x08 | 0x04); // LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF
+  lcd_command(0x04 | 0x02); // LCD_ENTRYMODESET | LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT
 
   lcd_command(0x01); // Clear display
   delay(2); // Useful after the clear to reduce screen glitches
